b2ac773317bd66cff4efeede86fceab32f7ad7dc
Rachelware <rachel.ware.ca@gmail.com>
2019-05-30 15:47:40 -0700

    fixed indentation to 4 spaces on files

--- src/main/java/com/google/codeu/servlets/LogoutServlet.java
+++ src/main/java/com/google/codeu/servlets/LogoutServlet.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,10 +30,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
-    String googleLogoutUrl = userService.createLogoutURL("/index.html");
-    response.sendRedirect(googleLogoutUrl);
-  }
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        String googleLogoutUrl = userService.createLogoutURL("/index.html");
+        response.sendRedirect(googleLogoutUrl);
+    }
 }
