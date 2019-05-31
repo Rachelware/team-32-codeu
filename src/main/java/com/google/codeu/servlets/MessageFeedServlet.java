5fd26b75667f988d57fcd0fe4415519aa71e5a67
Nicole Reinking <reinking.nicci@gmail.com>
2019-05-30 20:38:51 -0700

    added starter code for Message Feed Servlet

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,3 +1,23 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+
+import javax.servelet.http.HttpServelt;
+import javax.servelt.annotation.WebServelt;
+import javax.servelt.http.HttpServeletRequest;
+import javax.servelt.http.HttpServeletResponse;
+
+/* Handles fetching all messages for the public feed*/
+@WebServelt("/feed")
+public class MessageFeedServlet extends HttpServelt{
+
+    @Override
+	public void doGet(HttpServeletRequest request, HttpServeletResponse response)
+		
+	    throws IOException{
+
+	           response.getOutputStream().println("this will be my message feed");
+	           
+
+		}
+}
