25973b5396b7cd47997b226f9beef7b84a2e86a3
Nicole Reinking <reinking.nicci@gmail.com>
2019-05-30 20:48:52 -0700

    working message feed servlet that prints this will be my message feed

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -2,17 +2,17 @@ package com.google.codeu.servlets;
 
 import java.io.IOException;
 
-import javax.servelet.http.HttpServelt;
-import javax.servelt.annotation.WebServelt;
-import javax.servelt.http.HttpServeletRequest;
-import javax.servelt.http.HttpServeletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /* Handles fetching all messages for the public feed*/
-@WebServelt("/feed")
-public class MessageFeedServlet extends HttpServelt{
+@WebServlet("/feed")
+public class MessageFeedServlet extends HttpServlet{
 
     @Override
-	public void doGet(HttpServeletRequest request, HttpServeletResponse response)
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 		
 	    throws IOException{
 
