4e397e82b7365b8379e49c6cbe7b5c8374f50e68
Nicole Reinking <reinking.nicci@gmail.com>
2019-06-06 13:30:59 -0700

    put the js functions in html file

--- src/main/webapp/js/map-loader.js
+++
@@ -1,9 +0,0 @@
-/*
- * function to create the map
- */
-function createMap(){
-      const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.422, lng: -122.084},
-        zoom: 16
-      });
-    }
