be560951e6247603281b47e4881bcb8d735373b1
Nicole Reinking <reinking.nicci@gmail.com>
2019-06-05 21:38:47 -0700

    map javascript file

---
+++ src/main/webapp/js/map-loader.js
@@ -0,0 +1,9 @@
+/*
+ * function to create the map
+ */
+function createMap(){
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.422, lng: -122.084},
+        zoom: 16
+      });
+    }
