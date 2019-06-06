d7bba09f537d242934970c5483df2698498c49b8
Rachelware <rachel.ware.ca@gmail.com>
2019-06-06 08:37:01 -0700

    read in image input for messages

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -77,8 +77,12 @@ public class MessageServlet extends HttpServlet {
 
         String user = userService.getCurrentUser().getEmail();
         String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+        
+        String regex = "(https?://\\S+\\.(png|jpg))";
+        String replacement = "<img src=\"$1\" />";
+        String textWithImagesReplaced = text.replaceAll(regex, replacement);
 
-        Message message = new Message(user, text);
+        Message message = new Message(user, textWithImagesReplaced);
         datastore.storeMessage(message);
 
         response.sendRedirect("/user-page.html?user=" + user);
