dc9bbb5d4deb1458b02e840eade7d923eacc1dea
miaboloix <MbMv2017>
2019-06-08 16:41:39 -0700

    displayed stat data on the chart

--- src/main/webapp/js/chart-loader.js
+++ src/main/webapp/js/chart-loader.js
@@ -1,38 +1,38 @@
-var messageCountElement;
-var userCountElement;
-var averageElement;
 
 function drawChart() {
     var book_data = new google.visualization.DataTable();
+
+    var messageCountElement;
+    var userCountElement;
+    var averageElement;
 
     //define columns for the DataTable instance
     book_data.addColumn('string', 'Site Statistics');
     book_data.addColumn('number', 'Number');
 
     //get data from JSON ur;
-    fetch('/stats').then(res => res.json()).then((stats) => {
-            messageCountElement = stats.messageCount;
-            userCountElement = stats.userCount;
-            averageElement = parseFloat(stats.average);
-    });
+    fetch('/stats').then(res => res.json())
+    .then((stats) => {
+        console.log("");
+        messageCountElement = stats.messageCount;
+        userCountElement = stats.userCount;
+        averageElement = parseFloat(stats.average);
 
-    //add data to book_data
-    book_data.addRows(
-        [
-            ["Mock", 6],
+        //add data to book_data
+        book_data.addRows(
+            [
             ["Total Messages", messageCountElement],
             ["Active Users", userCountElement],
-            ["Average # of Messages Per User", averageElement],
-            ["Mock2", 4],
-            ["Mock3", 2]
-        ]
-    );
+            ["Average # of Messages Per User", averageElement]
+            ]
+        );
 
-    var chart = new google.visualization.BarChart(document.getElementById('book_chart'));
-    var chart_options = {
-        width: 800,
-        height: 400
-    };
+        var chart = new google.visualization.BarChart(document.getElementById('book_chart'));
+        var chart_options = {
+            width: 800,
+            height: 400
+        };
 
-    chart.draw(book_data, chart_options);
+        chart.draw(book_data, chart_options);
+    });
 }
