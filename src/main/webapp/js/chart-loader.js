03e8cb8bcaca8e83b12cc3a072d82d20bb4400ae
miaboloix <MbMv2017>
2019-06-08 16:56:52 -0700

    started styling by changing bar color and adding a label

--- src/main/webapp/js/chart-loader.js
+++ src/main/webapp/js/chart-loader.js
@@ -1,14 +1,14 @@
 
 function drawChart() {
-    var book_data = new google.visualization.DataTable();
+    //var book_data = new google.visualization.DataTable();
 
     var messageCountElement;
     var userCountElement;
     var averageElement;
 
     //define columns for the DataTable instance
-    book_data.addColumn('string', 'Site Statistics');
-    book_data.addColumn('number', 'Number');
+    //book_data.addColumn('string', 'Site Statistics');
+    //book_data.addColumn('number', 'Number');
 
     //get data from JSON ur;
     fetch('/stats').then(res => res.json())
@@ -18,21 +18,30 @@ function drawChart() {
         userCountElement = stats.userCount;
         averageElement = parseFloat(stats.average);
 
-        //add data to book_data
-        book_data.addRows(
-            [
-            ["Total Messages", messageCountElement],
-            ["Active Users", userCountElement],
-            ["Average # of Messages Per User", averageElement]
-            ]
-        );
+        var data = google.visualization.arrayToDataTable([
+            ["Stat", "Number", { role: "style" } ],
+            ["Total Messages", messageCountElement, "color: #e5e4e2"],
+            ["Active Users", userCountElement, "color: #e5e4e2"],
+            ["Average # of Messages Per User", averageElement, "color: #e5e4e2"]
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+            { calc: "stringify",
+                sourceColumn: 1,
+                type: "string",
+                role: "annotation" },
+            2]);
+
 
         var chart = new google.visualization.BarChart(document.getElementById('book_chart'));
         var chart_options = {
+            title: "Site Statistics",
+            legend: { position: "none" },
             width: 800,
             height: 400
         };
 
-        chart.draw(book_data, chart_options);
+        chart.draw(view, chart_options);
     });
 }
