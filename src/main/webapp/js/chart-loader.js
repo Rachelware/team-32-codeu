4e85285f2fb717ce93c6bc2f87d3c44b5ddbea23
miaboloix <MbMv2017>
2019-06-08 16:27:35 -0700

    sucessfully pulled stat data

--- src/main/webapp/js/chart-loader.js
+++ src/main/webapp/js/chart-loader.js
@@ -1,28 +1,38 @@
-
+var messageCountElement;
+var userCountElement;
+var averageElement;
 
 function drawChart() {
     var book_data = new google.visualization.DataTable();
+
     //define columns for the DataTable instance
-    book_data.addColumn('string', 'Book Title');
-    book_data.addColumn('number', 'Votes');
+    book_data.addColumn('string', 'Site Statistics');
+    book_data.addColumn('number', 'Number');
+
+    //get data from JSON ur;
+    fetch('/stats').then(res => res.json()).then((stats) => {
+            messageCountElement = stats.messageCount;
+            userCountElement = stats.userCount;
+            averageElement = parseFloat(stats.average);
+    });
 
     //add data to book_data
-    book_data.addRows([
-        ["The Best We Could Do", 6],
-        ["Sing, Unburied, Sing", 10],
-        ["The Book of Unknown Americans", 7],
-        ["The 57 Bus", 4],
-        ["The Handmaid's Tale", 8]
-    ]);
+    book_data.addRows(
+        [
+            ["Mock", 6],
+            ["Total Messages", messageCountElement],
+            ["Active Users", userCountElement],
+            ["Average # of Messages Per User", averageElement],
+            ["Mock2", 4],
+            ["Mock3", 2]
+        ]
+    );
 
     var chart = new google.visualization.BarChart(document.getElementById('book_chart'));
     var chart_options = {
         width: 800,
         height: 400
     };
-    chart.draw(book_data, chart_options);
-}
 
-function loadAll() {
-    drawChart();
+    chart.draw(book_data, chart_options);
 }
