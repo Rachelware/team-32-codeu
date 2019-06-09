a562708bba7318b117df3ae8171a74c577bcd4e8
miaboloix <MbMv2017>
2019-06-08 18:07:53 -0700

    changed text color and bar colors

--- src/main/webapp/js/chart-loader.js
+++ src/main/webapp/js/chart-loader.js
@@ -1,14 +1,8 @@
 
 function drawChart() {
-    //var book_data = new google.visualization.DataTable();
-
     var messageCountElement;
     var userCountElement;
     var averageElement;
-
-    //define columns for the DataTable instance
-    //book_data.addColumn('string', 'Site Statistics');
-    //book_data.addColumn('number', 'Number');
 
     //get data from JSON ur;
     fetch('/stats').then(res => res.json())
@@ -19,7 +13,7 @@ function drawChart() {
         averageElement = parseFloat(stats.average);
 
         var data = google.visualization.arrayToDataTable([
-            ["Stat", "Number", { role: "style" } ],
+            ["Stats", "Number", { role: "style" } ],
             ["Total Messages", messageCountElement, "color: #e5e4e2"],
             ["Active Users", userCountElement, "color: #e5e4e2"],
             ["Average # of Messages Per User", averageElement, "color: #e5e4e2"]
@@ -34,14 +28,43 @@ function drawChart() {
             2]);
 
 
-        var chart = new google.visualization.BarChart(document.getElementById('book_chart'));
+        var chart = new google.charts.Bar(document.getElementById('book_chart'));
         var chart_options = {
-            title: "Site Statistics",
-            legend: { position: "none" },
             width: 800,
-            height: 400
+            colors: ['#ffffff'],
+            chart: {
+                title: ''
+            },
+            fontSize: 18,
+            bars: 'horizontal',
+            legend: { position: "none" },
+            hAxis: {
+                textStyle: {
+                    color: '#ffffff'
+                }
+            },
+            vAxis: {
+                textStyle: {
+                    color: '#ffffff'
+                }
+            },
+            axes: {
+                x: {
+                    0: { side: 'bottom', color: '#ffffff', label: ''} // Top x-axis.
+                },
+                y: {
+                    0: {label: ''}
+                }
+            },
+            backgroundColor: '#263238',
+            annotations: {
+                textStyle: {
+                    fontSize: 18,
+                    color: '#f2f3f5'
+                }
+            }
         };
 
-        chart.draw(view, chart_options);
+        chart.draw(view, google.charts.Bar.convertOptions(chart_options));
     });
 }
