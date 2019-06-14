c147453e163753be202d648fb0d11165673e108c
miaboloix <MbMv2017>
2019-06-14 13:55:11 -0700

    added level to User object

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -4,11 +4,17 @@ public class User {
     
     private String email;
     private String aboutMe;
+    private int level;
     
     public User(String email, String aboutMe) {
         this.email = email;
         this.aboutMe = aboutMe;
+        this.level = 1;
     }
+
+    public int getLevel() { return level; }
+
+    public void levelUp() { this.level++; }
     
     public String getEmail(){
         return email;
