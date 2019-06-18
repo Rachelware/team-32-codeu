9328811782de81f91671e21953966c87e0f0b509
miaboloix <MbMv2017>
2019-06-18 14:21:01 -0700

    added all relevant links to nav header

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -125,9 +125,63 @@ function buildMessageDiv(message) {
     return messageDiv;
 }
 
+/**
+ * Adds a login or logout link to the page, depending on whether the user is
+ * already logged in.
+ */
+function addLoginOrLogoutLinkToNavigation() {
+    const navigationElement = document.getElementById('navigation');
+    if (!navigationElement) {
+        console.warn('Navigation element not found!');
+        return;
+    }
+
+    fetch('/login-status')
+        .then((response) => {
+        return response.json();
+})
+.then((loginStatus) => {
+        if (loginStatus.isLoggedIn) {
+        navigationElement.appendChild(createListItem(createLink(
+            '/user-page.html?user=' + loginStatus.username, 'Your Page')));
+
+        navigationElement.appendChild(
+            createListItem(createLink('/logout', 'Logout')));
+    } else {
+        navigationElement.appendChild(
+            createListItem(createLink('/login', 'Login')));
+    }
+});
+}
+
+/**
+ * Creates an li element.
+ * @param {Element} childElement
+ * @return {Element} li element
+ */
+function createListItem(childElement) {
+    const listItemElement = document.createElement('li');
+    listItemElement.appendChild(childElement);
+    return listItemElement;
+}
+
+/**
+ * Creates an anchor element.
+ * @param {string} url
+ * @param {string} text
+ * @return {Element} Anchor element
+ */
+function createLink(url, text) {
+    const linkElement = document.createElement('a');
+    linkElement.appendChild(document.createTextNode(text));
+    linkElement.href = url;
+    return linkElement;
+}
+
 /** Fetches data and populates the UI of the page. */
 function buildUI() {
     setPageTitle();
+    addLoginOrLogoutLinkToNavigation();
     showMessageFormIfViewingSelf();
     fetchMessages();
     fetchAboutMe();
