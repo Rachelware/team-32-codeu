cf5d3f021512c0a399be0360d818f7c6f729882f
miaboloix <MbMv2017>
2019-07-14 17:21:13 -0700

    map has bugs but it shows up

--- src/main/webapp/js/scavenger-map-loader.js
+++ src/main/webapp/js/scavenger-map-loader.js
@@ -1,131 +1,51 @@
+var map;
+var markers = [];
+var clicks = 0;
+
 function createMap(){
     const map = new google.maps.Map(document.getElementById('map'), {
-
-        //Google Plex office
         center: {lat: 37.422, lng: -122.084},
-        zoom: 14,
-
-        //Styles map in Night Mode
-       /* styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-                featureType: 'administrative.locality',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-            },
-            {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [{color: '#263c3f'}]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#6b9a76'}]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{color: '#38414e'}]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#212a37'}]
-            },
-            {
-                featureType: 'road',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#9ca5b3'}]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [{color: '#746855'}]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#1f2835'}]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#f3d19c'}]
-            },
-            {
-                featureType: 'transit',
-                elementType: 'geometry',
-                stylers: [{color: '#2f3948'}]
-            },
-            {
-                featureType: 'transit.station',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
-            },
-            {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{color: '#17263c'}]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#515c6d'}]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.stroke',
-                stylers: [{color: '#17263c'}]
-            }] */
+        zoom: 2,
     });
-/*
-//adds a landmark at Google West Campus
-    addLandmark(map, 37.423829, -122.092154, 'Google West Campus',
-        'Google West Campus is home to YouTube and Maps.')
 
-//adds a landmark at Stan the T-Rex
-    addLandmark(map, 37.421903, -122.084674, 'Stan the T-Rex',
-        'This is Stan, the T-Rex statue.')
-
-//adds a landmark Permanente Creek Trail
-    addLandmark(map, 37.420919, -122.086619, 'Permanente Creek Trail',
-        'Permanente Creek Trail connects Google to a system of bike trails.'); */
-
-//event listener listens for click then places marker
-    map.addListener('click', function(e) {
-        placeMarkerAndPanTo(e.latLng, map);
+    map.addListener('click', function(event) {
+        clicks++;
+        if (clicks <= 3) {
+            addMarker(event.latLng);
+        } else {
+            //TODO: Add popup
+        }
     });
 }
 
-
-/** Adds a marker that shows an info window when clicked.
-function addLandmark(map, lat, lng, title, description){
-    const marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        map: map,
-        title: title
-    });
-    const infoWindow = new google.maps.InfoWindow({
-        content: description
-    });
-    marker.addListener('click', function() {
-        infoWindow.open(map, marker);
-    });
-} */
-
-/*Places a marker at the location*/
-function placeMarkerAndPanTo(latLng, map) {
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    //creates marker
     var marker = new google.maps.Marker({
-        position: latLng,
+        position: location,
         map: map
     });
-    map.panTo(latLng);
+    //creates info window
+    var infoWindow = new google.maps.InfoWindow({
+        content: 'Answer #' + clicks
+    });
+    //defines when to show info window
+    marker.addListener('mouseover', function() {
+        infoWindow.open(marker.get('map'), marker);
+    });
+    markers.push(marker);
+    setMapOnAll(map);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    setMapOnAll(null);
+    markers = [];
 }
