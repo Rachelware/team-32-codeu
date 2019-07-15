2cf42588c78b19c460fdedf54afa7e94afdd514d
miaboloix <MbMv2017>
2019-07-15 15:56:28 -0700

    markers and info windows now work

--- src/main/webapp/js/scavenger-map-loader.js
+++ src/main/webapp/js/scavenger-map-loader.js
@@ -2,39 +2,40 @@ var map;
 var markers = [];
 var clicks = 0;
 
-function createMap(){
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.422, lng: -122.084},
+function initMap() {
+    var haightAshbury = {lat: 37.769, lng: -122.446};
+
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
+        center: haightAshbury,
+        mapTypeId: 'terrain'
     });
 
+    // This event listener will call addMarker() when the map is clicked.
     map.addListener('click', function(event) {
-        clicks++;
-        if (clicks <= 3) {
-            addMarker(event.latLng);
-        } else {
-            //TODO: Add popup
-        }
+        addMarker(event.latLng);
     });
+
 }
 
 // Adds a marker to the map and push to the array.
 function addMarker(location) {
-    //creates marker
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-    //creates info window
-    var infoWindow = new google.maps.InfoWindow({
-        content: 'Answer #' + clicks
-    });
-    //defines when to show info window
-    marker.addListener('mouseover', function() {
-        infoWindow.open(marker.get('map'), marker);
-    });
-    markers.push(marker);
-    setMapOnAll(map);
+    clicks++;
+    if (clicks <= 3) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        markers.push(marker);
+        var infoWindow = new google.maps.InfoWindow({
+            content: 'Answer to Clue #' + clicks
+        });
+        marker.addListener('mouseover', function() { //changed name from addEventListener to show description
+            infoWindow.open(marker.get('map'), marker);
+        });
+    } else {
+        window.alert("You have already created 3 markers. Please delete your existing markers to change your answers!");
+    }
 }
 
 // Sets the map on all markers in the array.
@@ -44,8 +45,19 @@ function setMapOnAll(map) {
     }
 }
 
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    clicks = 0;
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
 // Deletes all markers in the array by removing references to them.
 function deleteMarkers() {
-    setMapOnAll(null);
+    clearMarkers();
     markers = [];
 }
